32c1ef6040315941c50735203605e8b689af2b81
hellowupeng <hellowupeng@gmail.com>
2017-06-20 22:01:19 +0800

    .

---
+++ Ruby基础教程/chapter 1/bigger_smaller.rb
@@ -0,0 +1,22 @@
+a = 20
+if a >= 10 then
+  print "bigger\n"
+end
+if a <= 9 then
+  print "smaller\n"
+end
+# 省略 then
+a = 20
+if a >= 10
+  print "bigger\n"
+end
+if a <= 9
+  print "smaller\n"
+end
+# else
+a = 9
+if a >= 10
+  print "bigger\n"
+else
+  print "smaller\n"
+end
---
+++ Ruby基础教程/chapter 2/2.1.rb
@@ -0,0 +1,4 @@
+names = ["小林", "林", "高野", "森冈"]
+emptyArray = []
+names[0]
+print "第一个名字为：", names[0], "。\n"
